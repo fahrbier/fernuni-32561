8bf0cec7ed5ea667d3a06e452e473a5132a3fd62
Holger Lersch <holger@lersch.net>
2022-03-03 19:09:52 -0500

    ausgabe

--- spielwiese/klausur-ss2020-3d.c
+++ spielwiese/klausur-ss2020-3d.c
@@ -36,13 +36,34 @@ BSTL ein() {
    return b;
 }
 
+
+
+void aus(BSTL b) {
+    printf("%i\t%i\t%.2f\t", b.anz, b.artnr, b.price);
+    printf("%i.%i.%i\n", b.bestelldatum.d, b.bestelldatum.m, b.bestelldatum.y);
+}
+
 int main() 
 {
     int anzBes = 0;
-    BSTL bestListe[10];
+    int maxBest = 1;
+    float minPrice = 0.0;
+    BSTL bestListe[maxBest];
     
-    while (anzBes <= 10) {
+    while (anzBes <= maxBest) {
         bestListe[anzBes] =  ein();
+        anzBes++;
+    }
+    anzBes = 0;
+
+   printf("Minimalpreis?");
+   scanf("%f", &minPrice);   
+
+    printf("Anzahl\tArtNr\tPreis\tDatum\n");
+    while (anzBes <= maxBest) {
+        if (bestListe[anzBes].price >= minPrice) {
+           aus(bestListe[anzBes]);
+        }
         anzBes++;
     }
 
